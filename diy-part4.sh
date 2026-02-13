ffb1081a68bf391e93640f1bbe9ce42c6ebd1331
Can-666 <cacan666@163.com>
2026-02-13 21:51:54 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
